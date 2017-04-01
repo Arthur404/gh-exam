8dd241cc0399c7e9f30621055cf1bbbca60d132a
Arthur404 <ananarben@gmail.com>
2017-04-01 11:28:52 +0300

    style heder added

--- wp-content/themes/gh-exam/header.php
+++ wp-content/themes/gh-exam/header.php
@@ -15,26 +15,26 @@
 
 <body <?php body_class(); ?>>
 <div id="page" class="site">
-    <div class="container">
-        <header id="masthead" class="site-header" role="banner">
-                <div class="nav-header">
-                    <h1 class="logo">
-                        <?php the_custom_logo(); ?>
-                        BusinessPlus
-                    </h1>
-                    <div class="tel"><a href="tel:<?php echo get_theme_mod('phone_number'); ?>"><?php echo get_theme_mod('phone_number'); ?></a></div>
-                </div>
-                <nav class="main-nav">
-                    <button class="nav-btn open-btn" id="open-nav">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <button class="nav-btn close-btn" id="close-nav">
-                        <i class="fa fa-times"></i>
-                    </button>
-                    <?php wp_nav_menu(array('them_location' => 'menu-1', 'container' => false, 'menu_class' => 'navigation')) ?>
-                </nav>
-        </header>
-    </div>
+    <header id="masthead" class="site-header" role="banner">
+        <div class="container">
+            <div class="nav-header">
+                <h1 class="logo">
+                    <?php the_custom_logo(); ?>
+                    BusinessPlus
+                </h1>
+                <div class="tel"><a href="tel:<?php echo get_theme_mod('phone_number'); ?>"><?php echo get_theme_mod('phone_number'); ?></a></div>
+            </div>
+            <nav class="main-nav">
+                <button class="nav-btn open-btn" id="open-nav">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <button class="nav-btn close-btn remove-btn" id="close-nav">
+                    <i class="fa fa-times"></i>
+                </button>
+                <?php wp_nav_menu(array('them_location' => 'menu-1', 'container' => false, 'menu_class' => 'navigation')) ?>
+            </nav>
+        </div>
+    </header>
 
     <section class="hero">
         <div class="container header-page">
--- wp-content/themes/gh-exam/inc/customizer.php
+++ wp-content/themes/gh-exam/inc/customizer.php
@@ -41,8 +41,70 @@ function gh_exam_customize_register( $wp_customize ) {
             'type' => 'tel'
         )
     );
+    $wp_customize->add_setting(
+        'bg-hero'
+    );
+    $wp_customize->add_control(
+        new WP_Customize_Image_Control(
+            $wp_customize,
+            'bg-hero',
+            array(
+                'label' => esc_html__('Background image', 'gh-exam'),
+                'section' => 'hero-section'
+            )
+        )
+    );
+    /*--------------------------------------------------------------
+    # Blog page panel
+    --------------------------------------------------------------*/
+    $wp_customize->add_panel( 'blog_page', array(
+        'priority' => 10,
+        'capability' => 'edit_theme_options',
+        'theme_supports' => '',
+        'title' => __( 'Blog page', 'gh-exam' ),
+        'description' => __( 'Settings of blog page.', 'gh-exam' ),
+    ) );
+
+    /*--------------------------------------------------------------
+    # Hero blog section
+    --------------------------------------------------------------*/
+    $wp_customize->add_section(
+        'hero-blog-section',
+        array(
+            'title' => esc_html__( 'Hero blog settings', 'gh-exam' ),
+            'priority' => 10,
+            'panel' => 'blog_page',
+        )
+    );
+    $wp_customize->add_setting(
+        'bg-hero-blog'
+    );
+    $wp_customize->add_control(
+        new WP_Customize_Image_Control(
+            $wp_customize,
+            'bg-hero-blog',
+            array(
+                'label' => esc_html__('Background image', 'gh-exam'),
+                'section' => 'hero-blog-section'
+            )
+        )
+    );
 }
 add_action( 'customize_register', 'gh_exam_customize_register' );
+
+function custom_style() {
+    ?>
+    <style type="text/css">
+        .home .hero {
+            background: url(<?php echo get_theme_mod('bg-hero') ?>) 50% 0 /cover no-repeat, #000;
+        }
+        .blog .hero {
+            background: url(<?php echo get_theme_mod('bg-hero-blog') ?>) 50% 0 /cover no-repeat, #000;
+        }
+    </style>
+    <?php
+}
+add_action('wp_head', 'custom_style');
 
 function gh_exam_customize_preview_js() {
 	wp_enqueue_script( 'gh_exam_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
